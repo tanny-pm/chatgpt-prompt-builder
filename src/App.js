35b5505eea4d1b03f4bc5e35c9211e97122e5218
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-04-08 11:26:16 +0900

    Add: 固定のプロンプトを編集する機能

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [language, setLanguage] = useState("");
+
+  const hundleWordChange = (e) => {
+    setWord(e.target.value);
+  };
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const generatePrompt = () => {
+    return `${word}を${language}に翻訳してください。`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            label="翻訳したい言葉"
+            value={word}
+            onChange={hundleWordChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="言語"
+            value={language}
+            onChange={handleLanguageChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>{generatePrompt()}</Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
